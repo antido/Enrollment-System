32ab4a36b6379076a19fb29d0d70907adb43b989
antido <christantido@gmail.com>
2018-05-02 20:39:24 +0800

    Feature-Subject-Module-Templates

--- application/controllers/subject_controller.php
+++ application/controllers/subject_controller.php
@@ -8,5 +8,15 @@ class Subject_Controller extends CI_Controller{
 		$info['data'] = $this->subject_model->display_subjects_offered('subjects');
 		$this->load->view('subjects/subject_offered', $info);
 	}
+
+	public function subjects_enrolled()
+	{
+		$this->load->view('subjects/subject_enrolled');
+	}
+
+	public function subjects_finished()
+	{
+		$this->load->view('subjects/subject_finished');
+	}
 }
 ?>
--- application/views/subjects/subject_enrolled.php
+++ application/views/subjects/subject_enrolled.php
@@ -15,8 +15,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 	<header>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-			<a class="navbar-brand" href="<?php echo site_url('subject_controller/subjects_offered'); ?>">Subjects</a>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-success">
+			<a class="navbar-brand" href="<?php echo site_url('subject_controller/subjects_enrolled'); ?>">Subjects</a>
 			<div class="collapse navbar-collapse">
 				<ul class="navbar-nav">
 					<li class="nav-item">
@@ -29,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<main>
 		<div class="container-fluid mt-5">
-			<table class="table table-striped table-bordered" id="subjectOfferedTable">
+			<table class="table table-striped table-bordered" id="enrolledSubjectTable">
 				<thead class="thead-dark">
 					<tr>
 						<th>Subject ID</th>
@@ -40,44 +40,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</tr>
 				</thead>
 				<tbody>
-				<?php foreach($data as $row) { ?>
 					<tr>
-						<td><?php echo $row->subject_id; ?></td>
-						<td><?php echo $row->subject_name; ?></td>
-						<td><?php echo $row->subject_description; ?></td>
-						<td><?php echo $row->subject_unit; ?></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
 						<td>
-							<a class="btn btn-secondary" href="#viewSubjectModal" data-toggle="modal">View</a>
-							<a class="btn btn-success" href="">Enroll</a>
+							<a class="btn btn-secondary" href="#">View</a>
+							<a class="btn btn-danger" onclick="return confirm('Are you sure?');" href="#">Unenroll</a>
 						</td>
 					</tr>
-				<?php } ?>
 				</tbody>
 			</table>
 		</div>
 	</main>
 
-	<!-- View Subject Data Modal -->
-	<div class="modal fade" id="viewSubjectModal" tabindex="-1" role="dialog" aria-labelledby="viewSubjectModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-dialog-centered" role="document">
-			<div class="modal-content"> 
-				 <div class="modal-header">
-				 	<h5 class="modal-title" id="viewSubjectModalLabel">View Subject Data</h5>
-				 	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				 		<span aria-hidden="true">&times;</span>
-				 	</button>
-				 </div>
-				 <div class="modal-body">
-				 	 
-				 </div>
-				 <div class="modal-footer">
-				 	<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-				 </div>
-			</div>
-		</div>
-	</div>
-
-	<footer class="footer mt-5">
+	<footer class="footer mt-5"> 
 		<div class="container-fluid">
 			<p class="text-center">Enrollment System &copy; <?php echo date('Y'); ?></p>
 		</div>
@@ -85,7 +63,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<script type="text/javascript">
 		$(document).ready(function(){
-			$('#subjectOfferedTable').DataTable();
+			$('#enrolledSubjectTable').DataTable();
 		});
 	</script>
 </body>
